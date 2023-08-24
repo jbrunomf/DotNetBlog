c9da908f78175b386f54d27f70fea5b51154556e
João Bruno <jbrunomf@outlook.com>
2023-08-23 21:19:26 -0400

    feat: adicionando novo endpoint no CategoryController.cs
    :bug:

---
+++ DotNetBlog/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetBlog.Controllers;
+
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("categories")]
+    public async Task<IActionResult> Index([FromServices]BlogDataContext context)
+    {
+        var categories = await context.Categories.ToListAsync();
+        return Ok(categories);
+    }
+}
