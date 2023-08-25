cae46a1db1f8d4e621ec354132237200f92c4b1f
João Bruno <jbrunomf@outlook.com>
2023-08-24 20:31:19 -0400

    Adicionando GetById e PostAsync.

--- DotNetBlog/Controllers/CategoryController.cs
+++ DotNetBlog/Controllers/CategoryController.cs
@@ -1,16 +1,40 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace DotNetBlog.Controllers;
 
 [ApiController]
 public class CategoryController : ControllerBase
 {
-    [HttpGet("categories")]
+    [HttpGet("/v1/categories")]
     public async Task<IActionResult> Index([FromServices]BlogDataContext context)
     {
         var categories = await context.Categories.ToListAsync();
         return Ok(categories);
+    }
+
+    [HttpGet("/v1/categories/{id:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int id,
+        [FromServices] BlogDataContext context)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(category);
+    }
+
+    [HttpPost("/v1/categories")]
+    public async Task<IActionResult> PostAsync([FromBody] Category category,
+        [FromServices] BlogDataContext context)
+    {
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
+        return Created($"v1/categories/{category.Id}", category);
     }
 }
