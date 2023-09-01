373f2d5c912dd25f8b8dd51b817afab57c51babe
João Bruno <jbrunomf@outlook.com>
2023-08-31 21:15:06 -0400

    feat: iniciando com as validações.

--- DotNetBlog/ViewModels/EditorCategoryViewModel.cs
+++ DotNetBlog/ViewModels/EditorCategoryViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace DotNetBlog.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetBlog.ViewModels;
 
 public class EditorCategoryViewModel
 {
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre {2} e {1} caracteres.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "O slug é obrigatório.")]
     public string Slug { get; set; }
 }
