5bf83099e5fcdeba373dd34e7a8e3d6e3d3aa908
João Bruno <jbrunomf@outlook.com>
2023-09-11 21:15:41 -0400

    testando authorization a partir de roles no métodos GET.

--- DotNetBlog/Controllers/AccountController.cs
+++ DotNetBlog/Controllers/AccountController.cs
@@ -1,15 +1,38 @@
 ﻿using DotNetBlog.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetBlog.Controllers;
 
 [ApiController]
+[Route("v1")]
 public class AccountController : ControllerBase
 {
-    [HttpPost("v1/login")]
+    [HttpPost("login")]
     public IActionResult Login([FromServices]TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
         return Ok(token);
+    }
+
+    [Authorize(Roles = "user")]
+    [HttpGet("user")]
+    public IActionResult Getuser()
+    {
+        return Ok(User.Identity.Name);
+    }
+
+    [Authorize(Roles = "author")]
+    [HttpGet("author")]
+    public IActionResult GetAuthor()
+    {
+        return Ok(User.Identity.Name);
+    }
+
+    [Authorize(Roles = "admin")]
+    [HttpGet("admin")]
+    public IActionResult GetAdmin()
+    {
+        return Ok(User.Identity.Name);
     }
 }
