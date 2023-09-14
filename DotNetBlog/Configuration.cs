a5609ed671d6815e2fed33556e42c9376a537621
João Bruno <jbrunomf@outlook.com>
2023-09-13 21:23:11 -0400

    Organizando Program.cs e appsettings.json

--- DotNetBlog/Configuration.cs
+++ DotNetBlog/Configuration.cs
@@ -2,7 +2,15 @@
 
 public static class Configuration
 {
-    public static string JwtKey = "Mjk4OGQ5Y2EtZmZkMC00YjY0LWJiZGYtYTYyZGVkZTExMDE4";
-    public static string ApiKeyName = "api_key";
-    public static string ApiKey = "chave_teste_api_key";
+    public static string JwtKey = "";
+    public static string ApiKeyName = "";
+    public static string ApiKey = "";
+    public static SmtpConfiguration Smtp = new(); 
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password  { get; set; }
+    }
 }
