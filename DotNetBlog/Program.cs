dd81aebaf62aeb1274336d279c9e22a169eb0fa9
João Bruno <jbrunomf@outlook.com>
2023-09-14 20:54:44 -0400

    organizando código.

--- DotNetBlog/Program.cs
+++ DotNetBlog/Program.cs
@@ -21,11 +21,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
 
 app.Run();
