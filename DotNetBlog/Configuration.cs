8aec9a6cb0cbb5b036aad1243ad6613be6b5db4a
João Bruno <jbrunomf@outlook.com>
2023-10-02 19:26:10 -0400

    feat: método que retorna postagens

--- DotNetBlog/Configuration.cs
+++ DotNetBlog/Configuration.cs
@@ -2,9 +2,9 @@
 
 public static class Configuration
 {
-    public static string JwtKey = "";
-    public static string ApiKeyName = "";
-    public static string ApiKey = "";
+    public static string JwtKey = "chavejwt";
+    public static string ApiKeyName = "api-key";
+    public static string ApiKey = "api-key-teste";
     public static SmtpConfiguration Smtp = new(); 
     public class SmtpConfiguration
     {
---
+++ DotNetBlog/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using DotNetBlog.Data;
+using DotNetBlog.ViewModels;
+using DotNetBlog.ViewModels.Posts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetBlog.Controllers;
+
+[ApiController]
+public class PostController : ControllerBase
+{
+    [HttpGet("v1/posts")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context)
+    {
+        var posts = await context
+            .Posts
+            .AsNoTracking()
+            .Include(x => x.Category)
+            .Include(x => x.Author)
+            .Select(x => new ListPostViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Slug = x.Slug,
+                LastUpdateDate = x.LastUpdateDate,
+                Category = x.Category.Name,
+                Author = x.Author.Name
+            }).ToListAsync();
+        return Ok(new ResultViewModel<List<ListPostViewModel>>(posts));
+    }
+}
